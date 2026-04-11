1905ef0439a405ee9b2f0283f36f68d4bcf6ed8e
Ruthwik <ruthwikdasyam@gmail.com>
2026-04-11 11:55:32 -0700

    program md + precommit fixes

--- autoresearch/go2_cpu/_startup_patch.py
+++ autoresearch/go2_cpu/_startup_patch.py
@@ -1,6 +1,19 @@
+# Copyright 2026 Dimensional Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-import dimos.protocol.service.lcmservice as _lcm_mod
 import dimos.protocol.rpc.pubsubrpc as _rpc_mod
+import dimos.protocol.service.lcmservice as _lcm_mod
 
 # 1. Increase LCM polling timeout: 50ms -> 200ms
 #    750 threads × 20 polls/sec = 15k context switches/sec
