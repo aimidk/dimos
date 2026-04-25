97340b9b9f3f27f3e5d4626c2e39d7862e566fac
Aimi K <aimidk@users.noreply.github.com>
2026-04-25 09:30:13 +0800

    chore: add __fork_author__ to distinguish fork from upstream contributors

--- dimos/__init__.py
+++ dimos/__init__.py
@@ -6,6 +6,9 @@ dimensional awareness and modular agent-based architectures.
 Fork notes:
     Forked for personal learning and experimentation with agent architectures.
     Upstream: https://github.com/dimensionalOS/dimos
+
+Personal changes:
+    - Added __fork_author__ to track fork ownership separately from upstream contributors.
 """
 
 from importlib.metadata import PackageNotFoundError, version
@@ -16,12 +19,14 @@ except PackageNotFoundError:
     __version__ = "0.0.0.dev0"
 
 __author__ = "dimos contributors"
+__fork_author__ = "personal fork"
 __license__ = "Apache-2.0"
 __upstream__ = "https://github.com/dimensionalOS/dimos"
 
 __all__ = [
     "__version__",
     "__author__",
+    "__fork_author__",
     "__license__",
     "__upstream__",
 ]
